108c00cf0fc7d8a03d1c2f0cc09afe350a10d5c3
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-07-29 12:01:05 -0500

    Added a prompt to adjust box dimensions

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
+const page = document.querySelector("body")
 const container = document.querySelector(".container")
+container.style.cssText = 'display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16,1fr);'
 
 let divs;
 
-for (let i = 0; i < 16; i++){
-    for (let j = 0; j < 16; j++){
+for (let i = 0; i < 16 * 16; i++){
         divs = document.createElement('div');
         divs.classList.add('box');
         container.appendChild(divs);
-    }
+    
 }
+
+const button = document.createElement('button');
+page.appendChild(button);
+
+button.addEventListener('click', () => {
+    let x = prompt('Pick a number')
+    container.style.cssText = `display: grid; grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${x},1fr);`
+    container.innerHTML = ''
+    for (let i = 0; i < x * x; i++){
+        divs = document.createElement('div');
+        divs.classList.add('box');
+        container.appendChild(divs);
+}
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+    box.addEventListener('mouseover', () => {
+        box.style.background = "black"
+    })
+})
+})
+
+
 
 const boxes = document.querySelectorAll(".box");
 
@@ -17,3 +41,4 @@ boxes.forEach((box) => {
         box.style.background = "black"
     })
 })
+
